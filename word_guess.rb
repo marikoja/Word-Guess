6b3d4d2318e1238647ee71d746c02233eeb78971
Mariko <marikoanderson@gmail.com>
2018-02-12 16:12:09 -0800

    add Answer class and compares guess to answer method

--- word_guess.rb
+++ word_guess.rb
@@ -1,10 +1,10 @@
-
+require "awesome_print"
 class UserGuess
   def initialize
     @guess = gets.chomp.downcase
   end
 
-  attr_accessor :guess
+  attr_accessor :guess, :correct_guess, :wrong_guess
 
   def validate_letters
     until @guess =~ (/[a-zA-Z]/) && @guess.length == 1
@@ -13,9 +13,31 @@ class UserGuess
     end
   end
 
+  def compare_to_answer(answer_word)
+    @correct_guess = []
+    @wrong_guess = []
+    if answer_word.word.include?(@guess)
+      @correct_guess << @guess
+    else
+      @wrong_guess << @guess
+    end
+  end
+
+end
+
+class Answer
+  attr_accessor :word, :letters, :length
+  def initialize
+    words = ["testing"]
+    @word = words.sample
+    @letters = @word.split(//).uniq
+    @length = @word.length
+  end
 end
 
 test = UserGuess.new
 puts test.validate_letters
 
 puts test.guess
+puts test.compare_to_answer(Answer.new)
+ap test.wrong_guess, color: {string: :cyanish}
