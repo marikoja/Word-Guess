9d13c25009fecea8c0076e75bdcf19c5807bd4d9
Mariko <marikoanderson@gmail.com>
2018-02-12 17:14:57 -0800

    start to combine class and create user interface

--- word_guess.rb
+++ word_guess.rb
@@ -40,9 +40,27 @@ class Answer
 end
 
 class GameBoard
+  attr_accessor :lives_counter
   def initialize
     @main_image = "some main image that doesn't change"
     @lives_counter = 4
+  end
+
+  def losing_lives(guess)
+    @lives_counter = @lives_counter - (guess.wrong_guess).length
+  end
+
+  def lives_counter_display
+    @lives_counter.times do
+      print "some image"
+    end
+  end
+
+  def starting_display(answer)
+    lives_counter_display
+    puts @main_image
+    print answer.correct_array
+    puts ""
   end
 
   def display_gameboard(answer, guess)
@@ -54,11 +72,28 @@ class GameBoard
   end
 end
 
-test = UserGuess.new
-puts test.validate_letters
+# puts test.validate_letters
 
-puts test.guess
-puts test.compare_to_answer(Answer.new)
-ap test.wrong_guess, color: {string: :cyanish}
-gameboard = GameBoard.new
-gameboard.display_gameboard(Answer.new, test)
+# puts test.guess
+# puts test.compare_to_answer(Answer.new)
+# ap test.wrong_guess, color: {string: :cyanish}
+
+def word_guess_method
+  answer = Answer.new
+  gameboard = GameBoard.new
+  gameboard.starting_display(answer)
+  print "Please enter your single letter guess:"
+  user_guess = UserGuess.new
+  user_guess.compare_to_answer(answer)
+  gameboard.losing_lives(user_guess)
+  while gameboard.lives_counter > 0
+    gameboard.display_gameboard(answer, user_guess)
+    print "Please enter your single letter guess:"
+    user_guess = UserGuess.new
+    user_guess.compare_to_answer(answer)
+    gameboard.losing_lives(user_guess)
+  end
+end
+
+
+word_guess_method
